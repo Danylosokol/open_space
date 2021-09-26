9d0e95518a6176c31bb08dfdeddf957d832c32ac
Danylosokol <dansokol677@gmail.com>
2021-09-26 10:25:48 +0200

    finised password checker

--- main.js
+++ main.js
@@ -1,15 +1,32 @@
-let state = true;
-stateOfPanel(state);
+document.addEventListener('DOMContentLoaded', function(){
+    let password = "TrustNo1";
+    let state = true;
+    let okButton = document.getElementById("ok-button");
+    stateOfPanel();
 
-function stateOfPanel(state){
-    let launchButton = document.getElementById("launch-button");
-    launchButton.disabled = state;
-    let checkboxes = document.querySelectorAll("input[type=checkbox]");
-    for (let i = 0; i < checkboxes.length; i++) {
-        checkboxes[i].disabled = state;
+    okButton.addEventListener("click", function () {
+        let input = document.querySelector("input[type=password]").value;
+        if (input !== "") {
+            if (input === password) {
+                state = false;
+                stateOfPanel();
+            }
+        }
+    })
+
+    function stateOfPanel() {
+        let passwordInput = document.querySelector("input[type=password]");
+        passwordInput.disabled = !state;
+        okButton.disabled = !state;
+        let launchButton = document.getElementById("launch-button");
+        launchButton.disabled = state;
+        let checkboxes = document.querySelectorAll("input[type=checkbox]");
+        for (let i = 0; i < checkboxes.length; i++) {
+            checkboxes[i].disabled = state;
+        }
+        let ranges = document.querySelectorAll("input[type=range]");
+        for (let i = 0; i < ranges.length; i++) {
+            ranges[i].disabled = state;
+        }
     }
-    let ranges = document.querySelectorAll("input[type=range]");
-    for (let i = 0; i < ranges.length; i++) {
-        ranges[i].disabled = state;
-    }
-}
+});
