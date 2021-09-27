04ab3ddb01a326ad3018751c4e7fb87e3dd963f8
Danylosokol <dansokol677@gmail.com>
2021-09-27 08:26:49 +0200

    started fifth task, started eventListener function for checkboxes

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function(){
     let password = "TrustNo1";
     let state = true;
     let okButton = document.getElementById("ok-button");
+    let checkboxes = document.querySelectorAll("input[type=checkbox]");
     stateOfPanel();
 
     okButton.addEventListener("click", function () {
@@ -14,13 +15,23 @@ document.addEventListener('DOMContentLoaded', function(){
         }
     })
 
+    for(let i = 0; i < checkboxes.length; i++){
+        checkboxes[i].addEventListener("change", function(){
+            if(this.checked) {
+                console.log("checked");
+            }
+            else{
+                console.log("unchecked");
+            }
+        })
+    }
+
     function stateOfPanel() {
         let passwordInput = document.querySelector("input[type=password]");
         passwordInput.disabled = !state;
         okButton.disabled = !state;
         let launchButton = document.getElementById("launch-button");
         launchButton.disabled = state;
-        let checkboxes = document.querySelectorAll("input[type=checkbox]");
         for (let i = 0; i < checkboxes.length; i++) {
             checkboxes[i].disabled = state;
         }
