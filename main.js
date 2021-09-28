3e212193474f51b4c35e7c7b3cc6fd0035357123
Danylosokol <dansokol677@gmail.com>
2021-09-28 08:27:59 +0200

    finiched functions for checkboxes and ranges check and started launchRocket() function

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 document.addEventListener('DOMContentLoaded', function(){
-    let password = "TrustNo1";
+    let password = "1";
     let state = true;
     let okButton = document.getElementById("ok-button");
     let checkboxes = document.querySelectorAll("input[type=checkbox]");
+    let ranges = document.querySelectorAll("input[type=range]");
+    let checkboxesOn = false;
+    let rangesOn = false;
     stateOfPanel();
 
     okButton.addEventListener("click", function () {
@@ -17,13 +20,20 @@ document.addEventListener('DOMContentLoaded', function(){
 
     for(let i = 0; i < checkboxes.length; i++){
         checkboxes[i].addEventListener("change", function(){
-            if(this.checked) {
-                console.log("checked");
-            }
-            else{
-                console.log("unchecked");
+            checkboxesOn = stateOfCheckboxes();
+            if(checkboxesOn === true){
+                stateOfLaunchButton();
             }
         })
+    }
+
+    for(let i = 0; i < ranges.length; i++){
+        ranges[i].onchange = function(){
+            rangesOn = stateOfRanges()
+            if(rangesOn === true){
+                stateOfLaunchButton();
+            }
+        }
     }
 
     function stateOfPanel() {
@@ -31,13 +41,43 @@ document.addEventListener('DOMContentLoaded', function(){
         passwordInput.disabled = !state;
         okButton.disabled = !state;
         let launchButton = document.getElementById("launch-button");
-        launchButton.disabled = state;
+        launchButton.disabled = true;
         for (let i = 0; i < checkboxes.length; i++) {
             checkboxes[i].disabled = state;
         }
-        let ranges = document.querySelectorAll("input[type=range]");
         for (let i = 0; i < ranges.length; i++) {
             ranges[i].disabled = state;
         }
+    }
+
+    function stateOfCheckboxes(){
+        for(let i = 0; i < checkboxes.length; i++){
+            if(!checkboxes[i].checked){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    function stateOfRanges(){
+        for(let i = 0; i < ranges.length; i++){
+            if(ranges[i].value < 100){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    function stateOfLaunchButton(){
+        console.log("we in");
+        if(checkboxesOn === true && rangesOn === true) {
+            let launchButton = document.getElementById("launch-button");
+            launchButton.disabled = false;
+            launchRocket();
+        }
+    }
+
+    function launchRocket(){
+        return 0;
     }
 });
